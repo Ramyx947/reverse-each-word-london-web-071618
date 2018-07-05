07c84847fd02645499ed521c4bee2099db8e0f65
Ramona M <r1mari@yahoo.com>
2018-07-05 17:37:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence1)
-  reversed=sentence1.split(" ").collect do |word|
-    word.reverse
-  end
-  reversed.join(" ")
-end
+#   reversed=sentence1.split(" ").collect do |word|
+#     word.reverse
+#   end
+#   reversed.join(" ")
+# end
 
 # def reverse_each_word(sentence1)
 #   reversed=sentence1.split(" ").collect do |word|
@@ -11,5 +11,11 @@ end
 #   end
 #   reversed.join(" ")
 # end
-
+dwarfs.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+    
+    sentence1.each_with_index do |name, index|
+      puts "#{index+1} #{name}"
+    end
+  end
   
